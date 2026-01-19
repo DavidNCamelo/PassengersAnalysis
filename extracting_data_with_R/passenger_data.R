4904ab8386aa155f4058dcfca33669185ad57d1c
David Camelo <NICOLAS.CAMELO28@outlook.com>
2026-01-19 10:28:40 -0500

    Add missing dependency, and new workflow into the script

--- extracting_data_with_R/passenger_data.R
+++ extracting_data_with_R/passenger_data.R
@@ -3,7 +3,7 @@
 # base URL
 base_url <- "https://www.datos.gov.co/resource/eh75-8ah6.csv"
 
-# Current year to reference data updates until 
+# Current year to reference data updates until
 # the mmoment where this code will be runned
 current_year <- lubridate::year(Sys.Date())
 
@@ -20,18 +20,31 @@ for (year in 2019:current_year) {
     "-01-01T00:00:00%27%20and%20%27",
     year,
     "-12-31T00:00:00%27",
-    "&$limit=10000000"
+    "&$limit=100000000"
   )
-  
+
   # Read directly the file
   df <- readr::read_csv(year_url, show_col_types = FALSE)
-  
+
   # Append into the main list
   dataframes[[as.character(year)]] <- df
 }
 
 # Joining in a single datafram
-data <- dplyr::bind_rows(dataframes)
+cols_fix <- c(
+  "municipio_origen_ruta",
+  "municipio_destino_ruta"
+)
+
+data <- dataframes |>
+  purrr::keep(~ nrow(.x) > 0) |>
+  purrr::map(
+    ~ dplyr::mutate(
+      .x,
+      dplyr::across(dplyr::all_of(cols_fix), as.character)
+    )
+  ) |>
+  dplyr::bind_rows()
 
 # Upper the column names
 names(data) <- toupper(names(data))
